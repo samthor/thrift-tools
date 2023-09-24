c9856e6e202d231fcfadb33045bd31aa7750cb84
Sam Thorogood <sam.thorogood@gmail.com>
2023-09-24 10:34:11 +1000

    export varint

--- src/deps/varint.ts
+++ src/deps/varint.ts
@@ -79,20 +79,6 @@ export function readZigZagVarint32(readByte: () => number): number {
 }
 
 /**
- * Decodes a varint32, returning the value and number of bytes consumed.
- */
-export function decodeVarint32(source: Uint8Array, offset = 0): { value: number; size: number } {
-  let size = 0;
-  const readByte = () => {
-    const byte = source[offset + size];
-    ++size;
-    return byte;
-  };
-  const value = readVarint32(readByte);
-  return { value, size };
-}
-
-/**
  * Returns a zigzag varint but probably throws at >53 bits (i.e., {@link Number.MAX_SAFE_INTEGER}).
  */
 export function readZigZagVarint53(readByte: () => number) {
--- src/entrypoint/index.ts
+++ src/entrypoint/index.ts
@@ -1,2 +1,5 @@
 export * from '../deps/reader.js';
 export * from '../deps/compiler-deps.js';
+
+// just in case the readers want it :shrug:
+export * from '../deps/varint.js';
